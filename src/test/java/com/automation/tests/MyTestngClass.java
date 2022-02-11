3cacabf700e1c1b3f186cbb43d46d42b26360a85
ajayrdev <akreddy0415@gmail.com>
2022-02-11 10:50:03 +0530

    adding TestNG plugin and update POM file

---
+++ src/test/java/com/automation/tests/MyTestngClass.java
@@ -0,0 +1,22 @@
+package com.automation.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyTestngClass {
+  @Test
+  
+  public void myTestCase1() {
+	  //Selenium script
+	  System.out.println("Welcome to testNG1!");
+  }
+  
+@Test
+  
+  public void myTestCase2() {
+	//Selenium script
+	  System.out.println("Welcome to testNG2!");
+	  //Assert.fail();
+  }
+
+}
--- src/test/java/com/exceptionhandling/ExceptionHandlingPractice.java
+++ src/test/java/com/exceptionhandling/ExceptionHandlingPractice.java
@@ -1,7 +1,6 @@
 package com.exceptionhandling;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
